50f567c2d6f018971583fadf0756c47f2b784073
NOKAYA Jean-Michel <nokaya.jeanmichel@gmail.com>
2021-12-23 23:20:55 +0100

    done

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,21 +1,36 @@
+import DefaultPicture from "../../assets/profile.png"
 import "../../styles/Card.css"
 import PropTypes from 'prop-types'
+import styled from "styled-components"
+
+const CardLabel = styled.span`
+    color: #5843e4;
+    font-size: 22px;
+    font-weight: bold;
+`
+const CardImage = styled.img`
+    height: 80px;
+    width: 80px;
+    border-radius: 50%;
+`
 export default function Card({ label, title, picture }) {
     return (
         <div className="lmj-card">
             <span>{label}</span>
-            <img src={picture} alt="freelance" height={80} width={80} />
-            <span>{title}</span>
+            <CardImage src={picture} alt="freelance" />
+            <CardLabel>{title}</CardLabel>
         </div>
     )
 }
 
 Card.propTypes = {
-    label: PropTypes.string,
+    label: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
-    picture: PropTypes.string
+    picture: PropTypes.string.isRequired
 }
 
 Card.defaultProps = {
-    title: "Titre par défaut"
+    label: "",
+    title: "",
+    picture: DefaultPicture
 }
