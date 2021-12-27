514e33a346e22d162a22dde51e24bac6de7ba7c4
NOKAYA Jean-Michel <nokaya.jeanmichel@gmail.com>
2021-12-27 17:44:53 +0100

    first work

--- src/pages/Results/index.jsx
+++ src/pages/Results/index.jsx
@@ -12,7 +12,7 @@ const ResultsContainer = styled.div`
   margin: 60px 90px;
   padding: 30px;
   background-color: ${({ theme }) =>
-    theme === 'light' ? colors.backgroundLight : colors.backgroundDark};
+        theme === 'light' ? colors.backgroundLight : colors.backgroundDark};
 `
 
 const ResultsTitle = styled.h2`
@@ -32,7 +32,7 @@ const DescriptionWrapper = styled.div`
 
 const JobTitle = styled.span`
   color: ${({ theme }) =>
-    theme === 'light' ? colors.primary : colors.backgroundLight};
+        theme === 'light' ? colors.primary : colors.backgroundLight};
   text-transform: capitalize;
 `
 
@@ -53,66 +53,72 @@ const LoaderWrapper = styled.div`
 `
 
 function formatFetchParams(answers) {
-  const answerNumbers = Object.keys(answers)
+    const answerNumbers = Object.keys(answers)
 
-  return answerNumbers.reduce((previousParams, answerNumber, index) => {
-    const isFirstParam = index === 0
-    const separator = isFirstParam ? '' : '&'
-    return `${previousParams}${separator}a${answerNumber}=${answers[answerNumber]}`
-  }, '')
+    return answerNumbers.reduce((previousParams, answerNumber, index) => {
+        const isFirstParam = index === 0
+        const separator = isFirstParam ? '' : '&'
+        return `${previousParams}${separator}a${answerNumber}=${answers[answerNumber]}`
+    }, '')
+}
+
+export function formatJobList(title, listLength, index) {
+    if (index === listLength - 1) {
+        return title
+    }
+    return `${title},`
 }
 
 function Results() {
-  const { theme } = useTheme()
-  const { answers } = useContext(SurveyContext)
-  const fetchParams = formatFetchParams(answers)
+    const { theme } = useTheme()
+    const { answers } = useContext(SurveyContext)
+    const fetchParams = formatFetchParams(answers)
 
-  const { data, isLoading, error } = useFetch(
-    `http://localhost:8000/results?${fetchParams}`
-  )
+    const { data, isLoading, error } = useFetch(
+        `http://localhost:8000/results?${fetchParams}`
+    )
 
-  if (error) {
-    return <span>Il y a un problème</span>
-  }
+    if (error) {
+        return <span>Il y a un problème</span>
+    }
 
-  const resultsData = data?.resultsData
+    const resultsData = data?.resultsData
 
-  return isLoading ? (
-    <LoaderWrapper>
-      <Loader />
-    </LoaderWrapper>
-  ) : (
-    <ResultsContainer theme={theme}>
-      <ResultsTitle theme={theme}>
-        Les compétences dont vous avez besoin :
-        {resultsData &&
-          resultsData.map((result, index) => (
-            <JobTitle
-              key={`result-title-${index}-${result.title}`}
-              theme={theme}
-            >
-              {result.title}
-              {index === resultsData.length - 1 ? '' : ','}
-            </JobTitle>
-          ))}
-      </ResultsTitle>
-      <StyledLink $isFullLink to="/freelances">
-        Découvrez nos profils
-      </StyledLink>
-      <DescriptionWrapper>
-        {resultsData &&
-          resultsData.map((result, index) => (
-            <JobDescription
-              theme={theme}
-              key={`result-detail-${index}-${result.title}`}
-            >
-              <JobTitle theme={theme}>{result.title}</JobTitle>
-              <p>{result.description}</p>
-            </JobDescription>
-          ))}
-      </DescriptionWrapper>
-    </ResultsContainer>
-  )
+    return isLoading ? (
+        <LoaderWrapper>
+            <Loader />
+        </LoaderWrapper>
+    ) : (
+        <ResultsContainer theme={theme}>
+            <ResultsTitle theme={theme}>
+                Les compétences dont vous avez besoin :
+                {resultsData &&
+                    resultsData.map((result, index) => (
+                        <JobTitle
+                            key={`result-title-${index}-${result.title}`}
+                            theme={theme}
+                        >
+                            {formatJobList(result.title)}
+                        </JobTitle>
+                    ))}
+            </ResultsTitle>
+            <StyledLink $isFullLink to="/freelances">
+                Découvrez nos profils
+            </StyledLink>
+            <DescriptionWrapper>
+                {resultsData &&
+                    resultsData.map((result, index) => (
+                        <JobDescription
+                            theme={theme}
+                            key={`result-detail-${index}-${result.title}`}
+                        >
+                            <JobTitle theme={theme}>{result.title}</JobTitle>
+                            <p>{result.description}</p>
+                        </JobDescription>
+                    ))}
+            </DescriptionWrapper>
+        </ResultsContainer>
+    )
 }
 
 export default Results
