ee541e6cc25410b29d645d3ff8542e437c697684
NOKAYA Jean-Michel <nokaya.jeanmichel@gmail.com>
2022-01-08 21:24:10 +0100

    correction + react-reduc added to main package.json

--- src/app/store.js
+++ src/app/store.js
@@ -2,7 +2,7 @@ import { default as themeReducer } from "../features/theme/theme";
 import { default as freelancesReducer } from '../features/freelances/freelances.reducer'
 import { default as freelanceReducer } from '../features/freelance/freelance.reducer'
 import { default as surveyReducer } from '../features/survey/survey.reducer'
-import { default as answerReducer } from '../features/anwsers'
+import { default as answerReducer } from '../features/answers'
 import { default as resultsReducer } from '../features/results'
 
 import { configureStore } from '@reduxjs/toolkit'
